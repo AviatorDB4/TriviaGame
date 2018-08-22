f3f12ec440380a550ddde15385a2445dc15735ea
David Barton <david@Davids-MacBook-Pro.local>
2018-08-21 17:50:00 -0700

    html/some css/psuedo js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// 1) Start Game and start timer
+
+// 2) Variable for Questions/CorrectAnswers/IncorrectAnswers
+
+// 3) Get question
+
+// 4) Get Possible Answers
+
+// 5) Check answers
+
+// 6) Log result
+
+// 7) Check time remaining "if time > 0 run loop again" else "if time = 0 show results"
